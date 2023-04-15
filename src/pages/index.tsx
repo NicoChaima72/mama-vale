52e7568d528cdc02d615db179d32edb31da02959
Nicolas Chaima <nikochaima72@gmail.com>
2023-04-14 23:00:00 -0400

    Add filter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,42 @@
-import { Box, Button, Table, Text, Title } from "@mantine/core";
+import { Box, Button, Table, Text, TextInput, Title } from "@mantine/core";
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "~/utils/api";
 import moment from "moment";
+import type { inferRouterOutputs } from "@trpc/server";
+import type { AppRouter } from "~/server/api/root";
+import { type ChangeEvent, useState } from "react";
+
+type routerOutput = inferRouterOutputs<AppRouter>;
 
 const Home: NextPage = () => {
-  const { data: users, isLoading: isUsersLoading } =
-    api.users.getAll.useQuery();
+  const [lastnameFilter, setLastnameFilter] = useState("");
+  const [usersData, setUsersData] = useState<routerOutput["users"]["getAll"]>(
+    []
+  );
+  const { data: users, isLoading: isUsersLoading } = api.users.getAll.useQuery(
+    undefined,
+    {
+      onSuccess: (users: routerOutput["users"]["getAll"]) => {
+        setUsersData(users);
+      },
+    }
+  );
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setLastnameFilter(e.target.value);
+    console.log(e.target.value);
+    if (users) {
+      setUsersData(() =>
+        users.filter(
+          (user) =>
+            user.lastNames.toLowerCase().indexOf(e.target.value.toLowerCase()) >
+            -1
+        )
+      );
+    }
+  };
   return (
     <>
       <Head>
@@ -30,47 +59,55 @@ const Home: NextPage = () => {
       {isUsersLoading ? (
         <Text mt={"1rem"}>Cargando...</Text>
       ) : (
-        <Table
-          striped
-          mt={"2rem"}
-        >
-          <thead>
-            <tr>
-              <th>RUT</th>
-              <th>Nombres</th>
-              <th>Apellidos</th>
-              <th>Dirección</th>
-              <th>Ciudad</th>
-              <th>Número</th>
-              <th>Email</th>
-              <th>Cumpleaños</th>
-              <th>Estado</th>
-              <th>Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users?.map((user) => (
-              <tr key={user.rut}>
-                <td>{user.rut}</td>
-                <td>{user.names}</td>
-                <td>{user.lastNames}</td>
-                <td>{user.address}</td>
-                <td>{user.city}</td>
-                <td>{user.phone}</td>
-                <td>{user.email}</td>
-                <td>
-                  {moment(new Date(user.birthday as Date)).format("DD/MM/yyyy")}
-                </td>
-                <td>{user.civilStatus}</td>
-                <td>
-                  <Link href={`/edit/${user.rut}`}>
-                    <Button size="xs">Editar</Button>
-                  </Link>
-                </td>
+        <>
+          <TextInput
+            label="Buscar por apellido"
+            mt={"1rem"}
+            size="sm"
+            value={lastnameFilter}
+            onChange={handleInputChange}
+          />
+          <Table striped mt={"2rem"}>
+            <thead>
+              <tr>
+                <th>RUT</th>
+                <th>Nombres</th>
+                <th>Apellidos</th>
+                <th>Dirección</th>
+                <th>Ciudad</th>
+                <th>Número</th>
+                <th>Email</th>
+                <th>Cumpleaños</th>
+                <th>Estado</th>
+                <th>Acciones</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
+            </thead>
+            <tbody>
+              {usersData.map((user) => (
+                <tr key={user.rut}>
+                  <td>{user.rut}</td>
+                  <td>{user.names}</td>
+                  <td>{user.lastNames}</td>
+                  <td>{user.address}</td>
+                  <td>{user.city}</td>
+                  <td>{user.phone}</td>
+                  <td>{user.email}</td>
+                  <td>
+                    {moment(new Date(user.birthday as Date)).format(
+                      "DD/MM/yyyy"
+                    )}
+                  </td>
+                  <td>{user.civilStatus}</td>
+                  <td>
+                    <Link href={`/edit/${user.rut}`}>
+                      <Button size="xs">Editar</Button>
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
       )}
     </>
   );
